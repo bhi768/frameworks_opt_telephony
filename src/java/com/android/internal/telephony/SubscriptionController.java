omnirom/android_frameworks_opt_telephony

MSIM:Fix iccid issue during android upgrade.
To resolve IllegalArgumentException ,add cardstate check and later
use the ICCID which received as part of addSubInfoRecord() when
Phone.getFullIccSerialNumber() returns null ICCID value.

Change-Id: If1eed8beb4e4db2b782184046f18ab4dd2bfc6de
CRs-Fixed: 2300837

src/java/com/android/internal/telephony/SubscriptionController.java
@@ -921,10 +921,17 @@ public class SubscriptionController extends ISub.Stub {
      */
     @Override
     public int addSubInfoRecord(String iccId, int slotIndex) {
-        String fullIccId = iccId;
+        String fullIccId;
         Phone phone = PhoneFactory.getPhone(slotIndex);
-        if (phone != null) {
+        UiccCard uiccCard = UiccController.getInstance().getUiccCardForSlot(slotIndex);
+        if (phone != null && uiccCard != null) {
             fullIccId = phone.getFullIccSerialNumber();
+            if (TextUtils.isEmpty(fullIccId)) {
+                fullIccId = iccId;
+            }
+        } else {
+            if (DBG) logdl("[addSubInfoRecord]- null fullIccId");
+            return -1;
         }
 
         if (DBG) logdl("[addSubInfoRecord]+ iccId:" + SubscriptionInfo.givePrintableIccid(iccId) +
@@ -974,7 +981,7 @@ public class SubscriptionController extends ISub.Stub {
 
                     if (oldIccId != null && oldIccId.length() != iccId.length()
                            && (oldIccId.equals(IccUtils.getDecimalSubstring(iccId))
-                                    || iccId.equals(IccUtils.stripTrailingFs(oldIccId)))) {
+                           || iccId.equalsIgnoreCase(IccUtils.stripTrailingFs(oldIccId)))) {
                         value.put(SubscriptionManager.ICC_ID, iccId);
                     }
 

bhi768/frameworks_opt_telephony

src/java/com/android/internal/telephony/SubscriptionController.java
@@ -1010,10 +1010,17 @@ public class SubscriptionController extends ISub.Stub {
     @Override
     public int addSubInfo(String uniqueId, String displayName, int slotIndex,
             int subscriptionType) {
-        String fullIccId = uniqueId;
+        String fullIccId;
         Phone phone = PhoneFactory.getPhone(slotIndex);
-        if (phone != null) {
+        UiccCard uiccCard = UiccController.getInstance().getUiccCardForSlot(slotIndex);
+        if (phone != null && uiccCard != null) {
             fullIccId = phone.getFullIccSerialNumber();
+            if (TextUtils.isEmpty(fullIccId)) {
+                fullIccId = uniqueId;
+            }
+        } else {
+            if (DBG) logdl("[addSubInfoRecord]- null fullIccId");
+            return -1;
         }
 
         if (DBG) {
@@ -1085,7 +1092,7 @@ public class SubscriptionController extends ISub.Stub {
 
                         if (oldIccId != null && oldIccId.length() != uniqueId.length()
                                 && (oldIccId.equals(IccUtils.getDecimalSubstring(uniqueId))
-                                        || uniqueId.equals(IccUtils.stripTrailingFs(oldIccId)))) {
+                                || uniqueId.equalsIgnoreCase(IccUtils.stripTrailingFs(oldIccId)))) {
                             value.put(SubscriptionManager.ICC_ID, uniqueId);
                         }
 